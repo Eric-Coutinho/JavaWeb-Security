bcb4c416b64de2742b450cb330812ad5cb8f20cc
Eric-Coutinho <Eric.Coutinhop@hotmail.com>
2025-06-08 15:19:02 -0300

    initializing java project

---
+++ src/main/java/com/javaweb/app001_Security_ListaTarefas/App001SecurityListaTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javaweb.app001_Security_ListaTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App001SecurityListaTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App001SecurityListaTarefasApplication.class, args);
+	}
+
+}
